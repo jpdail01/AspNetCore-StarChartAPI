0c8d1cc6d530bd39d9dbb3617e152bd2444c7179
Jason Dailey <jdailey@hchb.com>
2020-08-26 20:29:54 -0400

    through mod 4

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarChart.Controllers
+{
+    using StarChart.Data;
+
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
