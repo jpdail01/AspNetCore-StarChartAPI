d90738f4d71454ed04bbbf014dfedfa7d80c91a2
Jason Dailey <jdailey@hchb.com>
2020-08-28 15:39:17 -0400

    through mod 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -19,5 +19,52 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet(template: "{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = this._context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (celestialObject == null)
+            {
+                return this.NotFound();
+            }
+
+            celestialObject.Satellites = this._context.CelestialObjects.Where(w => w.OrbitedObjectId == celestialObject.Id).ToList();
+            return this.Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = this._context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (celestialObjects.Count == 0)
+            {
+                return this.NotFound();
+            }
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = this._context.CelestialObjects.Where(w => w.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            
+            return this.Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = this._context.CelestialObjects.ToList();
+            if (celestialObjects.Count == 0)
+            {
+                return this.NotFound();
+            }
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = this._context.CelestialObjects.Where(w => w.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return this.Ok(celestialObjects);
+        }
     }
 }
